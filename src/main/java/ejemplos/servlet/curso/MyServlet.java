4f8edf2e86e061c1b6970c677dee5ebb8d913379
Francisco <fagoug@gmail.com>
2024-02-12 20:56:47 +0100

    20240212_Tercer_Commit_Incluido_JSP

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -1,12 +1,13 @@
 package ejemplos.servlet.curso;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 @WebServlet(urlPatterns = "/myServlet")
 public class MyServlet extends HttpServlet {
@@ -14,21 +15,9 @@ public class MyServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		response.setContentType("Text/html");
-		PrintWriter out = response.getWriter();
+		RequestDispatcher dispacher=request.getRequestDispatcher("Nombre.jsp");
+		dispacher.forward(request,response);
 
-		// send HTML page to client
-		out.println("<html>");
-		out.println("<head><title>Actividad 1 - Servlets</title></head>");
-		out.println("<body>");
-		out.println("<h1>Solicitud nombre</h1>");
-		out.println("<form action=\"/myServlet2\" method\"get\">");
-		out.println("<label for=\"vNombre\">Escribe tu nombre:</label><br>");
-		out.println("<input type=\"text\" id=\"vNombre\" name=\"vNombre\"><br>");
-		out.println("<p>Pulse el botón para enviar</p>");
-		out.println("<input type=\"submit\" value=\"Enviar\">");
-		out.println("</form>");
-		out.println("</body></html>");
 	}
 
 }
---
+++ src/main/webapp/Nombre.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario con JSP</title>
+</head>
+<body>
+    <h1>Escribe tu nombre</h1><br>
+    <form action="myServlet2" method="get">
+        <label for="vNombre">Nombre:</label>
+        <input type="text" id="vNombre" name="vNombre"><br>
+        <p>Pulse el bot&oacute;n para enviar</p>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
